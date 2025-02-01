b1cc8e512eb50260803ddb501904a1923d1e9c2d
mj <mu8786@gmail.com>
2025-02-01 21:38:08 +0900

    feat: add /docs, shadcn for tailwindcss v4
    https://github.com/shadcn/app-tailwind-v4/tree/main

---
+++ app/docs/[slug]/page.tsx
@@ -0,0 +1,102 @@
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const slug = (await params).slug;
+  const { default: Doc, metadata } = await import(`@/docs/${slug}.mdx`);
+
+  return (
+    <div className="space-y-8">
+      <h1>{metadata.title}</h1>
+      <h1>{metadata.summary}</h1>
+      <h1>{metadata.period}</h1>
+      <Doc />
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  // const docs = getDocs("docs/career/digital-nutrition");
+
+  // return docs.map((doc) => ({
+  //   slug: doc.slug,
+  // }));
+
+  return [
+    {
+      slug: "nextjs",
+    },
+  ];
+}
+
+// export function generateMetadata({ params }) {
+//   let post = getBlogPosts().find((post) => post.slug === params.slug);
+//   if (!post) {
+//     return;
+//   }
+//
+//   let { title, publishedAt: publishedTime, summary: description, image } = post.metadata;
+//   let ogImage = image ? image : `${baseUrl}/og?title=${encodeURIComponent(title)}`;
+//
+//   return {
+//     title,
+//     description,
+//     openGraph: {
+//       title,
+//       description,
+//       type: "article",
+//       publishedTime,
+//       url: `${baseUrl}/blog/${post.slug}`,
+//       images: [
+//         {
+//           url: ogImage,
+//         },
+//       ],
+//     },
+//     twitter: {
+//       card: "summary_large_image",
+//       title,
+//       description,
+//       images: [ogImage],
+//     },
+//   };
+// }
+//
+// export default function Page({ params }: { params: { slug: string } }) {
+//     const doc = getDocs("docs/career/digital-nutrition").find((doc) => doc.slug === params.slug);
+//
+//     if (!doc) {
+//       notFound();
+//     }
+//
+//     return (
+//       <section>
+//         <script
+//           type="application/ld+json"
+//           suppressHydrationWarning
+//           dangerouslySetInnerHTML={{
+//             __html: JSON.stringify({
+//               "@context": "https://schema.org",
+//               "@type": "BlogPosting",
+//               headline: doc.metadata.title,
+//               datePublished: doc.metadata.publishedAt,
+//               dateModified: doc.metadata.publishedAt,
+//               description: doc.metadata.summary,
+//               image: doc.metadata.image
+//                 ? `${baseUrl}${doc.metadata.image}`
+//                 : `/og?title=${encodeURIComponent(doc.metadata.title)}`,
+//               url: `${baseUrl}/blog/${doc.slug}`,
+//               author: {
+//                 "@type": "Person",
+//                 name: "My Portfolio",
+//               },
+//             }),
+//           }}
+//         />
+//         <h1 className="title text-2xl font-semibold tracking-tighter">{doc.metadata.title}</h1>
+//         <div className="mt-2 mb-8 flex items-center justify-between text-sm">
+//           <p className="text-sm text-neutral-600 dark:text-neutral-400">{formatDate(doc.metadata.createdAt)}</p>
+//         </div>
+//         <article className="prose">
+//           <CustomMDX source={doc.content} />
+//         </article>
+//       </section>
+//   );
+// }
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,11 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => <h1 className="text-5xl">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-4xl">{children}</h2>,
+    h3: ({ children }) => <h3 className="text-3xl">{children}</h3>,
+    h4: ({ children }) => <h4 className="text-2xl">{children}</h4>,
+    h5: ({ children }) => <h4 className="text-xl">{children}</h4>,
     ...components,
   };
 }
