bef39ff3a18bc8a9ce71e0eccc10d9ebb146af3b
mj <mu8786@gmail.com>
2025-02-04 14:41:20 +0900

    feat: add shadcn mdx-components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,12 +1,47 @@
 import type { MDXComponents } from "mdx/types";
+import { cn } from "@/lib/utils";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => <h1 className="text-5xl">{children}</h1>,
-    h2: ({ children }) => <h2 className="text-4xl">{children}</h2>,
-    h3: ({ children }) => <h3 className="text-3xl">{children}</h3>,
-    h4: ({ children }) => <h4 className="text-2xl">{children}</h4>,
-    h5: ({ children }) => <h4 className="text-xl">{children}</h4>,
+    h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h1 className={cn("font-heading mt-2 scroll-m-20 text-4xl font-bold", className)} {...props} />
+    ),
+    h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h2
+        className={cn(
+          "font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0",
+          className,
+        )}
+        {...props}
+      />
+    ),
+    h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h3 className={cn("font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight", className)} {...props} />
+    ),
+    h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h4 className={cn("font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight", className)} {...props} />
+    ),
+    h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h5 className={cn("mt-8 scroll-m-20 text-lg font-semibold tracking-tight", className)} {...props} />
+    ),
+    h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+      <h6 className={cn("mt-8 scroll-m-20 text-base font-semibold tracking-tight", className)} {...props} />
+    ),
+    a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+      <a className={cn("font-medium underline underline-offset-4", className)} {...props} />
+    ),
+    p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
+      <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)} {...props} />
+    ),
+    ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
+      <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+    ),
+    ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
+      <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+    ),
+    li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+      <li className={cn("mt-2", className)} {...props} />
+    ),
     ...components,
   };
 }
