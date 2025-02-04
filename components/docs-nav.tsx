42560269ebdc95739052d7882334a18f6a2041fe
mj <mu8786@gmail.com>
2025-02-04 14:54:55 +0900

    feat: add sidebar to /docs

--- components/docs-nav.tsx
+++ components/docs-nav.tsx
@@ -5,9 +5,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 interface DocsConfig {
-  // mainNav: MainNavItem[];
-  sidebarNav: NavItemWithChildren[];
-  // chartsNav: SidebarNavItem[];
+  career: NavItemWithChildren[];
 }
 
 type NavItem = {
@@ -27,13 +25,14 @@ type NavItemWithChildren = NavItem & {
 export function DocsNav({ config }: { config: DocsConfig }) {
   const pathname = usePathname();
 
-  const items = config.sidebarNav;
+  const items = config.career;
 
   return items.length ? (
     <div className="flex flex-col gap-6">
       {items.map((item, index) => (
         <div key={index} className="flex flex-col gap-1">
-          <h4 className="rounded-md px-2 py-1 text-sm font-semibold">{item.title}</h4>
+          <h4 className="rounded-md px-2 py-1 text-sm text-gray-400">Career</h4>
+          <h4 className="rounded-md px-2 py-1 text-sm font-bold">{item.title}</h4>
           {item?.items?.length && <DocsNavItems items={item.items} pathname={pathname} />}
         </div>
       ))}
@@ -43,16 +42,16 @@ export function DocsNav({ config }: { config: DocsConfig }) {
 
 function DocsNavItems({ items, pathname }: { items: NavItemWithChildren[]; pathname: string | null }) {
   return items?.length ? (
-    <div className="grid grid-flow-row auto-rows-max gap-0.5 text-sm">
+    <div className="ml-4 border-l border-gray-200 pl-3 text-sm">
       {items.map((item, index) =>
         item.href && !item.disabled ? (
           <Link
             key={index}
             href={item.href}
             className={cn(
-              "group text-foreground hover:bg-accent hover:text-accent-foreground flex h-8 w-full items-center rounded-lg px-2 font-normal underline-offset-2",
+              "group text-foreground flex h-8 w-full items-center rounded-lg px-2 font-normal underline-offset-2 hover:text-indigo-700",
               item.disabled && "cursor-not-allowed opacity-60",
-              pathname === item.href && "bg-accent text-accent-foreground font-medium",
+              pathname === item.href && "font-medium text-indigo-700",
             )}
             target={item.external ? "_blank" : ""}
             rel={item.external ? "noreferrer" : ""}
